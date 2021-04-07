e3eb24b955eeab567a0756b7d555f1d273fe131d
rocketpage <81737834+rocketpage@users.noreply.github.com>
2021-04-07 13:56:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-self.addEventListener("install", function (event) {
-			event.waitUntil(
-				caches.open(cacheName).then(function (cache) {
-					return cache.addAll(
-        ["/", "img/mainbg.svg", "img/01_352x264.webp", "img/02_352x264.webp", "img/03_352x264.webp", "img/04_352x264.webp", "img/05_352x264.webp", "img/06_352x264.webp", "img/01_416x312.webp", "img/02_416x312.webp", "img/03_416x312.webp", "img/04_416x312.webp", "img/05_416x312.webp", "img/06_416x312.webp", "img/01_800x600.webp", "img/02_800x600.webp", "img/03_800x600.webp", "img/04_800x600.webp", "img/05_800x600.webp", "img/06_800x600.webp", "01_416x312.png", "manifest.json", "index.html"]).then(function () {
-						console.log("Success! App is available offline!")
-					})))
-			}
-		),
-		self.addEventListener("fetch", i => {
-			i.respondWith(caches.match(i.request).then(n => n || fetch(i.request)))
-		});
+// Promise.race is no good to us because it rejects if
+// a promise rejects before fulfilling. Let's make a proper
+// race function:
+function promiseAny(promises) {
+  return new Promise((resolve, reject) => {
+    // make sure promises are all promises
+    promises = promises.map((p) => Promise.resolve(p));
+    // resolve this promise as soon as one resolves
+    promises.forEach((p) => p.then(resolve));
+    // reject if all promises reject
+    promises.reduce((a, b) => a.catch(() => b)).catch(() => reject(Error('All failed')));
+  });
+}
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(promiseAny([caches.match(event.request), fetch(event.request)]));
+});
