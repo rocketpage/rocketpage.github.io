31ff87a01e9004b98d30aa4d062f914f2aee5320
rocketpage <81737834+rocketpage@users.noreply.github.com>
2021-04-08 05:57:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 this.addEventListener("install", function(event) {
     event.waitUntil(caches.open("v1").then(function(cache) {
-        return cache.addAll(["manifest.json", "index.html", "img/mainbg.svg", "img/01_352x264.webp", "img/02_352x264.webp", "img/03_352x264.webp", "img/04_352x264.webp", "img/05_352x264.webp", "img/06_352x264.webp", "img/01_416x312.webp", "img/02_416x312.webp", "img/03_416x312.webp", "img/04_416x312.webp", "img/05_416x312.webp", "img/06_416x312.webp"]).then(function() {
+        return cache.addAll(["/","manifest.json", "index.html", "img/mainbg.svg", "img/01_352x264.webp", "img/02_352x264.webp", "img/03_352x264.webp", "img/04_352x264.webp", "img/05_352x264.webp", "img/06_352x264.webp", "img/01_416x312.webp", "img/02_416x312.webp", "img/03_416x312.webp", "img/04_416x312.webp", "img/05_416x312.webp", "img/06_416x312.webp"]).then(function() {
             console.log("Success! App is available offline!");
         })
     }));
