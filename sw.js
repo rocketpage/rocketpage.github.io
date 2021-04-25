628c340e3220add4e9a8d5080e2c98c9a15e7bb4
rocketpage <81737834+rocketpage@users.noreply.github.com>
2021-04-25 06:41:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,43 @@
-self.oninstall = event => {
-  self.skipWaiting();
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+'/', 
+'img/01_416x312.webp', 
+'img/02_416x312.webp', 
+'img/03_416x312.webp', 
+'img/04_416x312.webp', 
+'img/05_416x312.webp', 
+'img/06_416x312.webp', 
+'img/01_800x600.webp', 
+'img/02_800x600.webp', 
+'img/03_800x600.webp', 
+'img/04_800x600.webp', 
+'img/05_800x600.webp', 
+'img/06_800x600.webp'
+];
 
-  const toCache = Array(300).fill('').map((v, i) => `img/${i}.webp`);
-  toCache.push('./');
-
+self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open('img').then(cache => cache.addAll(toCache))
-  );
-};
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );  
+});
 
-self.onfetch = event => {
+self.addEventListener('activate', function(event) {
+  console.log('Finally active. Ready to start serving content!');  
+});
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
-};
+});
