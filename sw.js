83462b05b8cda2dedb7df06846cf2211c78375c1
rocketpage <81737834+rocketpage@users.noreply.github.com>
2021-04-25 06:17:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-self.addEventListener("install",i=>{i.waitUntil(caches.open("v1").then(i=>i.addAll(["/","img/01_416x312.webp","img/02_416x312.webp","img/03_416x312.webp","img/04_416x312.webp","img/05_416x312.webp", "img/06_416x312.webp","img/01_800x600.webp","img/02_800x600.webp","img/03_800x600.webp","img/04_800x600.webp","img/05_800x600.webp","img/06_800x600.webp"]).then(function(){console.log("Success! App is available offline!")})))}),self.addEventListener("fetch",i=>{i.respondWith(caches.match(i.request).then(n=>n||fetch(i.request)))});
+self.oninstall = event => {
+  self.skipWaiting();
+
+  const toCache = Array(300).fill('').map((v, i) => `images/${i}.webp`);
+  toCache.push('./');
+
+  event.waitUntil(
+    caches.open('img-images').then(cache => cache.addAll(toCache))
+  );
+};
+
+self.onfetch = event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+};
