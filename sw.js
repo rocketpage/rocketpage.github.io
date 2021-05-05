9c86cb4958aa9da7406002ab09b912cfed4649c1
rocketpage <81737834+rocketpage@users.noreply.github.com>
2021-05-05 23:51:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,45 @@
-self.addEventListener("install",i=>{i.waitUntil(caches.open("v1").then(i=>i.addAll(["/","img/01_416x312.webp","img/02_416x312.webp","img/03_416x312.webp","img/04_416x312.webp","img/05_416x312.webp","img/06_416x312.webp","img/01_800x600.webp","img/02_800x600.webp","img/03_800x600.webp","img/04_800x600.webp","img/05_800x600.webp","img/06_800x600.webp","icons/favicon-16x16.png", "icons/favicon-32x32.png", "icons/apple-touch-icon.png", "icons/icon-72x72.png", "icons/icon-96x96.png", "icons/icon-128x128.png", "icons/icon-144x144.png", "icons/icon-152x152.png", "icons/icon-192x192.png", "icons/icon-384x384.png", "icons/icon-512x512.png", "icons/maskable_icon.png"]).then(function(){console.log("Success! App is available offline!")})))}),self.addEventListener("fetch",i=>{i.respondWith(caches.match(i.request).then(n=>n||fetch(i.request)))});
+const CACHE = "network-or-cache-v1";
+const timeout = 100;
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => cache.addAll([
+				"img/01_416x312.webp",
+				"img/02_416x312.webp",
+				"img/03_416x312.webp",
+				"img/04_416x312.webp",
+				"img/05_416x312.webp",
+				"img/06_416x312.webp",
+				"img/01_800x600.webp",
+				"img/02_800x600.webp",
+				"img/03_800x600.webp",
+				"img/04_800x600.webp",
+				"img/05_800x600.webp",
+				"img/06_800x600.webp"
+            ])
+        ));
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(fromNetwork(event.request, timeout)
+      .catch((err) => {
+          console.log(`Error: ${err.message()}`);
+          return fromCache(event.request);
+      }));
+});
+
+function fromNetwork(request, timeout) {
+    return new Promise((fulfill, reject) => {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then((response) => {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+}
+
+function fromCache(request) {
+    return caches.open(CACHE).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject("no-match")
+        ));
+}
