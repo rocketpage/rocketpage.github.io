4c436f3fbca2059662d90c62c8204a2e9672d8a3
rocketpage <81737834+rocketpage@users.noreply.github.com>
2021-08-19 12:43:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
-AMP_SW.init({
-  assetCachingOptions: [{
-    regexp: /\.(png|jpg|woff2|woff|css|js)/,
-    cachingStrategy: 'CACHE_FIRST',
-  }],
-  offlinePageOptions: {
-    url: '/offline.html',
-    assets: [],
-  },
-});
+const cacheName = 'demo_04'; const precacheResources = ['/']; self.addEventListener('install', (event) => { console.log('Service worker install event!'); event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources))); }); self.addEventListener('activate', (event) => { console.log('Service worker activate event!'); }); self.addEventListener('fetch', (event) => { console.log('Fetch intercepted for:', event.request.url); event.respondWith( caches.match(event.request).then((cachedResponse) => { if (cachedResponse) { return cachedResponse; } return fetch(event.request); }), ); });
