f2aaa339731f58b2ac734045be4c9981b3e4fb48
rocketpage <81737834+rocketpage@users.noreply.github.com>
2021-08-19 12:24:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
+importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+AMP_SW.init();
+
 const cacheName = 'demo_02'; const precacheResources = ['index.html']; self.addEventListener('install', (event) => { console.log('Service worker install event!'); event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources))); }); self.addEventListener('activate', (event) => { console.log('Service worker activate event!'); }); self.addEventListener('fetch', (event) => { console.log('Fetch intercepted for:', event.request.url); event.respondWith( caches.match(event.request).then((cachedResponse) => { if (cachedResponse) { return cachedResponse; } return fetch(event.request); }), ); });
